ff5bd6ef41e7106c4b5f092c8f33e13f4cd1024d
YassineBDH <boudouh.yassine@gmail.com>
2023-11-20 16:26:07 +0100

    jeuxCombat

---
+++ js/script.js
@@ -0,0 +1,20 @@
+let grandir = document.querySelector(".perso");
+grandir.addEventListener("click",function(){
+    grandir.style.width = '400px';
+    this.style.height = '600px';
+    this.style.position = 'relative';
+    this.style.right = '300%';
+
+
+});
+// grandir = document.querySelector(".perso");
+grandir.addEventListener("onclick",function(){
+    this.style.width = '130px';
+    this.style.height = '200px';
+    this.style.position = 'relative';
+    this.style.right = '30%';
+
+
+
+    // this.style.display = ;
+});
